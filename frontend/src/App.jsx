ebd586276d49723f5d735e0a57a7fe1e750dec06
Deepak <dkmdeepak8137@gmail.com>
2024-09-04 10:26:03 +0530

    'portfolio'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import '../src/bootstrap.min.css'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>Hello Beginner</h1>
+        <h1 className='roboto-regular'>Hello Beginner</h1>
       </div>
     </>
   )
